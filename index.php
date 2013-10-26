acca5e9d76285e566738f0a8253368c7c0ec345f
Pedro Flores <armandorf.05@gmail.com>
2013-10-26 14:54:20 -0700

    added entries, models, and views folders

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php
+    include_once("config/config.php");
+?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php echo(SITENAME) ?></title>
+    </head>
+    <body>
+        <?php
+            // put your code here
+        ?>
+    </body>
+</html>

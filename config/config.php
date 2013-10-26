f51a6eda57dec58d50895a2cd1c36159c891739d
Pedro Flores <armandorf.05@gmail.com>
2013-10-26 16:46:35 -0700

    added A. and Prieto

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 #  Config File:
 #  CS174_hw3
-#  Pedro A. Flores, Samira C. Oliva
+#  Pedro A. Flores Prieto, Samira C. Oliva
 
 
 <!--config.php should have configuration constants for things like

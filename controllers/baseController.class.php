1bbf848828a13391565c57fec10e6b264d2615ce
Samira C. Oliva <oliva@ieee.org>
2013-10-28 21:54:20 -0700

    baseController

---
+++ controllers/baseController.class.php
@@ -0,0 +1,18 @@
+<?php
+
+# base.class.php controller "landing controller"
+# Pedro A. Flores Prieto & Samira C. Oliva Madrigal
+# CS174_HW3
+
+
+abstract class baseController
+{
+ 
+   //selects appropriate view			
+	abstract function controller();
+	
+}
+
+?>
+
+
---
+++ controllers/main.class.php
@@ -0,0 +1,27 @@
+<?php
+
+# main.php controller
+# CS174_hw3
+# Pedro A. Flores Prieto, Samira C. Oliva Madrigal
+
+require_once('baseController.class.php');
+require_once('./models/featured.php);
+
+class mainController extends baseController
+{
+
+   public function getFeatured()
+   {
+
+	//call models
+
+   }
+
+}
+
+
+
+
+
+
+?>

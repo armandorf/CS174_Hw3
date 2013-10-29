a0d6d398a6a88cc2fb90dc321eb10008293067b8
Samira C. Oliva <oliva@ieee.org>
2013-10-28 20:31:53 -0700

    create.php edit

--- config/create.php
+++ config/create.php
@@ -1,27 +1,42 @@
+<?php
+
+/* 
 # Create File:
 # CS174_hw3
-# Pedro Flores, Samira C. Oliva
+# Pedro A. Flores Prieto, Samira C. Oliva Madrigal
+*/
 
+require_once("config.php");
+$dbName = 'POEMS_DB';
+    
+// CREATE NEW DATABASE
+$db_connect =  mysqli_connect("$host_name", "$admin_name", "$admin_passwd");
+    
+ // CONNECT TO A DATABASE & Check connection
+if (mysqli_connect_errno()) 
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
-<?php
-    include_once("config.php");
+// CREATE DATABASE
+$create_poems_db = "CREATE DATABASE $dbName";
+if (mysqli_query($db_connect, $create_poems_db)) 
+	echo "Database poems_db created successfully";
+else 
+	echo "Error creating poems database: " . mysqli_error($db_connect);
     
-    // CREATE NEW DATABASE
-    $db_connect = mysqli_connect("$host_name", "$admin_name", "$admin_passwd");
-    
-    // CONNECT TO A DATABASE // 
-    // Check connection
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-    // Create database
-    $create_poems_db = "CREATE DATABASE poems_db";
-    if (mysqli_query($db_connect, $create_poems_db)) {
-        echo "Database poems_db created successfully";
-    } else {
-        echo "Error creatingc database: " . mysqli_error($db_connect);
-    }
-    
-    // CREATE THE NECESSARY TABLES
-    
+// CREATE TABLES
+/* 
+# timeStamp: date the poem was created/uploaded
+# title: poem title
+# content: poem content 
+*/
+$table = "CREATE TABLE poems(timeStamp DATETIME NOT NULL, PRIMARY KEY (timeStamp), title VARCHAR(100), content VARCHAR(500));
+if(mysqli_query($db_connect, $table))
+	echo "Table was created successfully";
+else
+	echo "Error creating table in the database" . mysqli_error($db_connect);    
+// 
+
+// DATETIME example: YYYY-MM-DD HH:MM:SS, such as 2008-10-23 10:37:22
+
+ 
 ?>

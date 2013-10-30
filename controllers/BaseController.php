4e652bd0779020bccdbc2808e71918650ec7d377
Pedro Flores <armandorf.05@gmail.com>
2013-10-30 14:50:18 -0700

    made this class abstract, a prototype for its sub-classes

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -5,32 +5,10 @@
 
     require_once('./config/config.php');
 
-    class BaseController {
-
-        private $controller;
-
-        //selects appropriate view		
-        public function callView($view) {
-            $_SESSION['view'] = $view;
-            
-        }
-
-        function uploadNewPoem($title, $poem, $author, $timestamp, $starRating) {
-            // call appropriate method from model class
-        }
-
-        function getRandomPoem($randIDNum) {
-            
-        }
-
-        function getPoemByTitle($title) {
-            
-        }
-
-        public function setController($aControllerName) {
-            $controller = $aControllerName;
-        }
-
+    abstract class BaseController {
+        function load($sessionVar);
+        function callModel(); 
+        function drawView($view);
     }
 
 ?>

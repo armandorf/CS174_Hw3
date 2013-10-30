d452fbff1f4c091ef13750713d5fcb5c6dc8d2fa
Pedro Flores <armandorf.05@gmail.com>
2013-10-29 20:04:12 -0700

    some other changes

--- index.php
+++ index.php
@@ -9,31 +9,22 @@ session_start();
 include_once("./config/config.php");
 $controllers_available= array('main', 'upload', 'single_poem');
 $controller = ""; // will contain the controller that will call the proper view
-//
-//-------------------------------------------------------------
-//Determine the Controller to call to decide what view to draw
+
+//Determine the Controller to decide what view to draw
 if (isset($_GET['c']) && in_array($_GET['c'], $controllers_available))
 {
    $pivot = strtolower($_GET['c']);
    switch(strtolower($pivot)) {
         case "main":
-//          require_once(BASEURL.'/controllers/main.php');
             $controller = $pivot;
-            // draw($_SESSION['view']);
             break;
 
         case "upload":
-//            require_once(BASEURL.'/controllers/upload.php');
-//            uploadController();
-//            draw($_SESSION['main']);
             $controller = $pivot;
             break;
         
         case "single_poem":
             $controller = $pivot;
-//            require_once(BASEURL.'/controllers/single_poem.php');
-//            mainController();
-//            draw($_SESSION['view']);
             break;
             
         default:
@@ -43,10 +34,15 @@ if (isset($_GET['c']) && in_array($_GET['c'], $controllers_available))
 
 // decide what to do with $controller:
 // WELL, DRAW THE PROPER VIEW:
-// 
+
 
 ?> 
 
+<?php 
+    require_once("./config/config.php");
+    // test if database works
+    
+?>
 <?php 
     include_once './views/BaseView.php';
     $b = new BaseView;

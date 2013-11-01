96e833c4128a027024c655bc114df23c6db556af
Samira C. Oliva <oliva@ieee.org>
2013-10-31 23:25:41 -0700

    cr

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -4,7 +4,6 @@
 # Pedro A. Flores Prieto & Samira C. Oliva Madrigal
 # CS174_HW3
 
-<<<<<<< HEAD
 require_once('./config/config.php');
 
 
@@ -19,12 +18,3 @@ abstract class baseController {
 
 ?>
 
-=======
-    abstract class BaseController {
-        function load($sessionVar);
-        function callModel(); 
-        function drawView($view);
-    }
-
-?>
->>>>>>> d4f3164b27b84ac3c7f4cd0b465ae9caf420b060

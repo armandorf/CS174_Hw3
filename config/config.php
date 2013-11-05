a05364ea6b24c85dda019db82ba2e12d97032dd1
Samira C. Oliva <oliva@ieee.org>
2013-11-04 20:29:33 -0800

    config

--- config/config.php
+++ config/config.php
@@ -1,13 +1,14 @@
-
 <?php
-    # Config File:
-    # CS174_hw3
-    # Pedro A. Flores Prieto, Samira C. Oliva
+# Config:  Configuration File
+# Model: Featured
+# CS174_hw3
+# Created on 10/26/13.
+# Copyright (c) 2013 Pedro A. Flores Prieto, Samira C. Oliva Madrigal
 
     // dababase connection info
     $host_name = "localhost";
     $admin_name = "root";
-    $admin_passwd = "setVal";
+    $admin_passwd = "rosso99";
     
     //base URL for the website
     define("BASEURL", 'http://localhost/CS173_Hw3');
@@ -16,8 +17,11 @@
     define("SITENAME", "Looney Limericks");
 
     define("qDATABASE", "POEMS_DB");
-    $table = "poems";
+    $tableName = "poems";
+    $dbName = "POEMS_DB";
     
     $db_connect = mysqli_connect("$host_name", "$admin_name", "$admin_passwd", qDATABASE);
+    $db_connect_insert = mysqli_connect($host_name, $admin_name, $admin_passwd, $dbName);
+
 ?>
 

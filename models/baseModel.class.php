8c91ea184e697d25ec4e771ba1d881878b60a9fc
Samira C. Oliva <oliva@ieee.org>
2013-11-04 20:33:38 -0800

    baseModel

--- models/baseModel.class.php
+++ models/baseModel.class.php
@@ -1,21 +1,21 @@
 <?php
-    /*
-    # baseModel.class.php controller "landing controller"
-    # Pedro A. Flores Prieto & Samira C. Oliva Madrigal
-    # CS174_HW3
 
-    # Only model may access database and provide data to
-    # controllers. Controllers request information from models.
-    */
+# Model:  baseModel.class.php controller "landing controller"
+# CS174_hw3
+# Created on 10/26/13.
+# Copyright (c) 2013 Pedro A. Flores Prieto, Samira C. Oliva Madrigal
+# Assignment: Only model may access database and provide data to
+# controllers. Controllers request information from models.
+    
 
-    require_once(BASEURL.'/config/config.php');
+    require_once('./config/config.php');
 
-    class baseModel {
+abstract class baseModel {
 
-	function createTable();
-        function load();
-	function insertRecord();
-	function executeQuery();
-    }
+     abstract function insertRecord( $timeStamp, $title, $content, $author, $star);
+     abstract function executeQuery($query);   
+
+ }
 
 ?>
+
